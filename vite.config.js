5eba9054d1883608dd3ed28db9b2f0d5f65efb3f
ellamonacillo <ellamonacillo@gmail.com>
2025-01-28 13:48:18 +0800

    Folders

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/faq-accordion.github.io/', 
-  build: {
-    outDir: 'dist',
-  },
   plugins: [
     vue(),
     tailwindcss(),
